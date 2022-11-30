5dcac5b91d9fee2c29ec47db8e545ed6738ea419
Alexandr Makarov <makarena2009@yandex.ru>
2022-11-30 14:57:52 +0300

    homework

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.Write("a =  ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("b =  ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// int min = number2;
+// int max = number1;
+// if (number1 > number2) max = number1;
+// else min = number1;
+// if (number2 > number1) max = number2;
+// else min = number2;
+// Console.Write("max = ");
+// Console.WriteLine(max);
+// Console.Write("min = ");
+// Console.WriteLine(min);
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// int max = number1;
+// if (number1 > max) max = number1;
+// if (number2 > max) max = number2;
+// if (number3 > max) max = number3;
+
+// Console.Write("max= ");
+// Console.WriteLine(max);
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("Введите число: ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// if (numb % 2 == 0)
+// {
+//     Console.WriteLine("да");       
+// }
+//     else
+// {
+//     Console.WriteLine("нет");
+// }
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+// Console.WriteLine("Введите число : ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count = 1 ;
+// while (count <= number);
+//     if (number % 2 == 0)
+// {
+// Console.Write(count + " ");
+// count ++ ;
+// }
+
